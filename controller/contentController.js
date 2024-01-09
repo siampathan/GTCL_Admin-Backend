0101061e0c13b6af33d4e977517fd4c78f401cfb
siampathan005@gmail.com <siampathan005@gmail.com>
2024-01-09 16:56:12 +0600

    include update and delete oparation

--- controller/contentController.js
+++ controller/contentController.js
@@ -13,12 +13,13 @@ const getItemsById = async (req, res) => {
   try {
     const response = await Content.findOne({
       where: {
-        id: req.params.id,
+        _id: req.params.id,
       },
     });
     res.json(response);
   } catch (err) {
     console.error(err.message);
+    res.status(500).json({ msg: "Internal Server Error" });
   }
 };
 
@@ -55,17 +56,56 @@ const createItems = async (req, res) => {
 };
 
 const updateItems = async (req, res) => {
-  const content = await Content.findOne({
-    where: {
-      id: req.params.id,
-    },
-  });
+  const updatedData = {
+    _menuid: req.body._menuid,
+    _heading: req.body._heading,
+    _sub_heading: req.body._sub_heading,
+    _title: req.body._title,
+    _sub_title: req.body._sub_title,
+    _description: req.body._description,
+    _button: req.body._button,
+    _link: req.body._link,
+    _serial: req.body._serial,
+    _status: req.body._status,
+  };
 
-  if (!content) return res.status(404).json({ msg: "Data not Found" });
+  try {
+    const [rowsUpdated, [updatedItem]] = await Content.update(updatedData, {
+      where: { _id: req.params.id },
+    });
+
+    if (rowsUpdated > 0) {
+      res.status(200).json({ msg: "Item Updated Successfully", updatedItem });
+    } else {
+      res.status(404).json({ msg: "Item Not Found" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ msg: "Internal Server Error" });
+  }
+};
+
+const deleteItems = async (req, res) => {
+  try {
+    const rowsDeleted = await Content.destroy({
+      where: { _id: req.params.id },
+    });
+
+    if (rowsDeleted > 0) {
+      res.status(200).json({ msg: "Product Deleted Successfully" });
+    } else {
+      res.status(404).json({ msg: "Product Not Found" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ msg: "Internal Server Error" });
+  }
 };
 
 module.exports = {
   getItems,
   getItemsById,
   createItems,
+  updateItems,
+  deleteItems,
 };
--- routes/contentRoute.js
+++ routes/contentRoute.js
@@ -3,11 +3,15 @@ const {
   getItems,
   getItemsById,
   createItems,
+  updateItems,
+  deleteItems,
 } = require("../controller/contentController");
 const router = express.Router();
 
 router.get("/content", getItems);
 router.get("/content/:id", getItemsById);
 router.post("/content", createItems);
+router.patch("/content/:id", updateItems);
+router.delete("/content/:id", deleteItems);
 
 module.exports = router;
